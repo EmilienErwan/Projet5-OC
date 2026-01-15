d1834ca402bdfc2f3dd6f8ecf08d709038d1c730
Erwan Emilien <emilien.erwan@live.fr>
2026-01-15 21:24:27 +0100

    ajout css connection/inscription form et modif vue connection/inscription form

--- views/templates/inscriptionForm.php
+++ views/templates/inscriptionForm.php
@@ -5,17 +5,20 @@
 ?>
 
 <div class="inscription-form">
-    <form action="index.php?action=registerUser" method="post" class="">
-        <h2>Connexion</h2>
-        <div class="formGrid">
-            <label for="pseudo">Pseudo</label>
-            <input type="text" name="pseudo" id="pseudo" required>
-            <label for="email">Addresse email</label>
-            <input type="text" name="email" id="email" required>
-            <label for="password">Mot de passe</label>
-            <input type="password" name="password" id="password" required>
-            <button class="submit">S'inscrire</button>
-        </div>
-        <p>Déjà inscrit ? <a href="index.php?action=showConnectionForm">Connectez-vous</a></p>
-    </form>
+    <div class="formWrapper">
+        <form action="index.php?action=registerUser" method="post" class="">
+            <h2>Inscription</h2>
+            <div class="formGrid">
+                <label for="pseudo">Pseudo</label>
+                <input type="text" name="pseudo" id="pseudo" required>
+                <label for="email">Addresse email</label>
+                <input type="text" name="email" id="email" required>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password" required>
+                <button class="submit">S'inscrire</button>
+            </div>
+            <p>Déjà inscrit ? <a href="index.php?action=showConnectionForm">Connectez-vous</a></p>
+        </form>
+    </div>
+    <img src=".\uploads\images\logos\connection_image.png">
 </div>
